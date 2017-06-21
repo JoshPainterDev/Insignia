17570798f5f1fdff8c0660694d6e6fbededcff50
REAPER\Josh <joshxpainter@gmail.com>
2017-06-21 08:16:53 -0700

    asd

---
+++ Project_DAHYUN/Assets/Scripts/Crafting Scripts/CraftingManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShowMaterialOptions(int matNum)
+    {
+        if(matNum == 1)
+        {
+
+        }
+        else if(matNum == 2)
+        {
+
+        }
+        else if(matNum == 3)
+        {
+
+        }
+    }
+}
