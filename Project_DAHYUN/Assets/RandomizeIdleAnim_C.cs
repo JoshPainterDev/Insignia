6f06303e5f2c8ae1a70243fa1111549d4d63ba4c
REAPER\Josh <j.painter@digipen.edu>
2019-01-20 20:20:56 -0800

    aiediles

---
+++ Project_DAHYUN/Assets/RandomizeIdleAnim_C.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeIdleAnim_C : MonoBehaviour
+{
+    private float MAX = 0.5f;
+    private float MIN = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.GetComponent<Animator>().isActiveAndEnabled)
+        {
+            float rand = Random.Range(MIN, MAX);
+            StartCoroutine(StartIdle(rand));
+        }
+    }
+
+    IEnumerator StartIdle(float delay)
+    {
+        this.GetComponent<Animator>().enabled = false;
+        yield return new WaitForSeconds(delay);
+        this.GetComponent<Animator>().enabled = true;
+    }
+}
